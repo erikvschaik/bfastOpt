1b2370c09148ff711d49e51ec2a8b7e3c6df017d
Erik van Schaik <erikvschaik@gmail.com>
2016-01-01 12:14:30 +0100

    Test

--- R/bfastOpt.R
+++ R/bfastOpt.R
@@ -1,18 +1,14 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' @title bfastOpt
+#'
+#' @description Some test description
+#' @param A param
+#'
+#' @return ... Test
+#'
+#' @author E. van Schaik
+#'
 #' @import raster
+#' @export
 
 
 bfastOpt <- function(data=NULL, training.set=NULL, nIter=NULL) {
