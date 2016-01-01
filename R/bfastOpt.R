0ded8cc21ef566fbb697088760b04455fe1eab32
Erik van Schaik <erikvschaik@gmail.com>
2016-01-01 12:02:02 +0100

    Just for testing

--- R/bfastOpt.R
+++ R/bfastOpt.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -12,7 +12,10 @@
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
+#' @import raster
 
-hello <- function() {
-  print("Hello, world!")
+
+bfastOpt <- function(data=NULL, training.set=NULL, nIter=NULL) {
+  print('Hello World')
 }
+
